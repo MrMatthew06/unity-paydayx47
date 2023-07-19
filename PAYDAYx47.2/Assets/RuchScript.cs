84c23841291f1577b9a76c3a17373ce52334f0ef
MrMatthew06 <mrmatthew006@gmail.com>
2023-07-19 22:03:11 +0200

    pierwszy dzień
    - sterowanie

---
+++ PAYDAYx47.2/Assets/RuchScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+
+public class RuchScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    Vector2 ruch;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        ruch.x = Input.GetAxisRaw("Horizontal");
+        ruch.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.MovePosition(rb.position + ruch * speed * Time.fixedDeltaTime * 10 * 1.5f);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + ruch * speed * Time.fixedDeltaTime * 10);
+        }
+    }
+}
